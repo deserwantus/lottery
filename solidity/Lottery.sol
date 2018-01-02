4a35bbec94e1b273f53c65251f85347c5d930390
Dima Panchuk <d5panchuk@gmail.com>
2018-01-02 12:30:56 +0200

    .

--- solidity/Lottery.sol
+++ solidity/Lottery.sol
@@ -22,20 +22,22 @@ contract Ownable {
 contract Lottery is Ownable {
 
 	uint public ticketsCapacity;
-	uint public ticketPrice = 2 ether;
+	uint public ticketPrice;
 	uint public jackpot;
-	uint public jackpotIncrement = 1 ether;
+	uint public jackpotIncrement;
 	uint public smallWinMultiplier;
 
 	uint public jackpotWinnerNumber;
 
 	mapping(uint => address) public tickets;
-	mapping(address => uint) public winningMoney;
 
-	function Lottery(uint _ticketsCapacity, uint _smallWinMultiplier) Ownable() public {
+	function Lottery(uint _ticketsCapacity, uint _ticketPrice,
+	    uint _jackpotIncrement, uint _smallWinMultiplier) Ownable() public {
 
 		owner = msg.sender;
 		ticketsCapacity = _ticketsCapacity;
+		ticketPrice = _ticketPrice * 10**18;
+		jackpotIncrement = _jackpotIncrement * 10**18;
 		smallWinMultiplier = _smallWinMultiplier;
 	}
 
@@ -53,35 +55,28 @@ contract Lottery is Ownable {
 		tickets[ticketNumber] = msg.sender;
 		jackpot += jackpotIncrement;
 
-		winningMoney[owner] += ticketPrice / 4;
+		owner.transfer(ticketPrice / 4);
 	}
 
 	function play() onlyOwner public {
-		uint smallWinnersCapacity = ticketsCapacity / 100 * 5;
 
+		uint smallWinnersCapacity = ticketsCapacity / 100 * 5;
 		for(uint i = 1; i <= smallWinnersCapacity; i++) {
 			uint smallWinnerNumber = uint(block.blockhash(block.number-1)) % ticketsCapacity + 1;
             address smallWinner = tickets[smallWinnerNumber];
 			if(smallWinner != 0) {
-				winningMoney[smallWinner] += ticketPrice * smallWinMultiplier;
+			    uint smallWinnerMoney = ticketPrice * smallWinMultiplier;
+				smallWinner.transfer(smallWinnerMoney);
 			}
 		}
 
 		jackpotWinnerNumber = uint(block.blockhash(block.number-1)) % ticketsCapacity + 1;
 		address jackpotWinner = tickets[jackpotWinnerNumber];
 		if(jackpotWinner != 0) {
-		    winningMoney[jackpotWinner] += jackpot;
+		    uint jackpotWinnerMoney = jackpot;
 			jackpot = 0;
+			jackpotWinner.transfer(jackpotWinnerMoney);
 			clearTickets();
 		}
-	}
-
-	function withdraw() public {
-	    uint amount = winningMoney[msg.sender];
-
-	    if(amount > 0) {
-	        msg.sender.transfer(amount);
-	        winningMoney[msg.sender] = 0;
-	    }
 	}
 }
